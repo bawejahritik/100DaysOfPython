764f99db929df8d0cb2042ed1377dd5c28626ca9
bawejahritik <bawejahritik@gmail.com>
2023-09-01 17:02:10 -0400

    Day 9

---
+++ Day 9/blindauction.py
@@ -0,0 +1,16 @@
+import os
+user_choice = "yes"
+print("Welcome to the secret auction program.")
+
+bidders = {}
+
+while user_choice=="yes":
+    name = input("What is your name?: ")
+    bid = int(input("What's yoru bid?: $"))
+    user_choice = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    bidders[name] = bid
+    if user_choice=="yes":
+        os.system('cls')
+    else:
+        highest_bid = max(bidders, key= lambda k:bidders[k])
+        print(f"The winner is {highest_bid} with a bid of ${bidders[highest_bid]}")
